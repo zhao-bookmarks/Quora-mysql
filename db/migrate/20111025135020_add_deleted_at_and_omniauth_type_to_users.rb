5e20a128edb5d1d09b9904f6f3345a6adc624627
GitLabHQ Admin User <admin@local.host>
2012-02-13 10:44:12 +0800

    reset project

--- db/migrate/20111025135020_add_deleted_at_and_omniauth_type_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddDeletedAtAndOmniauthTypeToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :deleted_at, :timestamp
-    add_column :users, :omniauth_type, :string
-  end
-end
